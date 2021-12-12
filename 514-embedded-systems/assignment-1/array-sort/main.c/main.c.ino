bd36e9424bd5051994bfd165b07b2fd93ddd09c6
lilykiwi <Lily>
2021-12-12 16:43:24 +0000

    feat: :sparkles: add start of embedded final script s1

---
+++ 514-embedded-systems/assignment-1/array-sort/main.c/main.c.ino
@@ -0,0 +1,20 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  //pinMode(LED_BUILTIN, OUTPUT);
+
+  int data[50];
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  for (i = 0; i > 50; i++) {
+    data.push(random(255));
+
+    }
+  }
+}
+
+
+
+//https://www.tutorialspoint.com/cprogramming/c_arrays.htm
